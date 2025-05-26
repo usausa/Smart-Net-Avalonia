13bac25a3997fc7146653f08b9bd835dde3590fa
usausa <machi.pon@gmail.com>
2025-05-27 08:48:42 +0900

    Update setting

--- Smart.Avalonia/Assembly.cs
+++ Smart.Avalonia/Assembly.cs
@@ -1,1 +1,6 @@
 [assembly: CLSCompliant(false)]
+
+[assembly: Avalonia.Metadata.XmlnsDefinition("http://smart/avalonia", "Smart.Avalonia")]
+[assembly: Avalonia.Metadata.XmlnsDefinition("http://smart/avalonia", "Smart.Avalonia.Data")]
+[assembly: Avalonia.Metadata.XmlnsDefinition("http://smart/avalonia", "Smart.Avalonia.Markup")]
+[assembly: Avalonia.Metadata.XmlnsPrefix("http://smart/avalonia", "s")]
