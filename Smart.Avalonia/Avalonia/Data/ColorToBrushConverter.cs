5fbad84d0f66a5a3a86b13b70a57d7455f348528
usausa <machi.pon@gmail.com>
2026-09-23 16:52:25 +0900

    Update

--- Smart.Avalonia/Avalonia/Data/ColorToBrushConverter.cs
+++ Smart.Avalonia/Avalonia/Data/ColorToBrushConverter.cs
@@ -9,12 +9,12 @@ using global::Avalonia.Media;
 
 public sealed class ColorToBrushConverter : IValueConverter
 {
-    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         return value is Color color ? new SolidColorBrush(color) : AvaloniaProperty.UnsetValue;
     }
 
-    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         return value is SolidColorBrush brush ? brush.Color : BindingOperations.DoNothing;
     }
